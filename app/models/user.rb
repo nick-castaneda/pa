c9cbe690bd5ca47a61904619d4165256699cee11
Nicholas Castaneda <npcastaneda@gmail.com>
2015-12-31 16:18:08 -0800

    adding messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,40 @@
 class User < ActiveRecord::Base
-  # I do not understand exactly what this code is, but it makes sure
-  # that the user's email is unique and that the pw is at least 1 digit
-  before_save { self.email = email.downcase }
+
+  # validation rule that name must be present
+  # REGEX here's the breakdown of how this one works:
+  #         '/' starts the regex
+  #         '\A' matches the start of a string
+  #         '[\w+\-.]+' looks for at least one word character, plus,
+  #            hyphen, or dot
+  #         '@' is a literal "at sign"
+  #         '[a-z\d\-.]+' looks for at least one letter, digit, hyphen,
+  #            or dot
+  #         '\.' is a literal dot
+  #         '\z' matches the end of a string
+  #         '/' end of regex
+  #         '\.' case-insensitive
+  # validation rule that email must be present, the length must be less
+  # than 255 characters, and fit the regex format defined above.
+
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 1 }
+                                    format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: false }
+
+
+
+
+
+  # before_save { self.email = email.downcase }
+  # validates :name, presence: true, length: { maximum: 50 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, length: { maximum: 255 },
+  #                   format: { with: VALID_EMAIL_REGEX },
+  #                   uniqueness: { case_sensitive: false }
+  # has_secure_password
+  # validates :password, presence: true, length: { minimum: 1 }
 
   # I do not understand exactly what this code is, but I know it
   # encrpyts passwords
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,77 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+
+  # Setup method sets up a random user template? for tests.
+  def setup
+    @user = User.new(name: "Example User", email: "user@example")
+  end
+
+  # Not sure about this.
+  # # If the user is valid based on the user model, assert true.
+  # test "should be valid" do
+  #   assert_not @user.valid?
   # end
+
+  # This test sets the user name to multiple spaces
+  # If the user name is not present or just spaces, assert_not false
+  test "name should be present" do
+    @user.name = "       "
+    assert_not @user.valid?
+  end
+
+  # This test sets the user name to multiple spaces
+  # If the user name is not present or just spaces, assert_not false
+  test "email should be present" do
+    @user.email = "       "
+    assert_not @user.valid?
+  end
+
+  # This test sets the user name to a string of 51 characters
+  # If the user name is longer than 50 characters, assert_not false
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  # This test sets the email name to a string of 256 characters
+  # If the user email is longer than 255 characters, assert_not false
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  # This test sets valid_addresses equal to an array of valid email
+  # addresses
+  # If the each of the email addresses is valid, assert true
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[nick@casta.com NICK@casta.COM N_IC-K@casta.ne.da n+ick@casta.ne]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  # This test sets invalid_addresses to an array of invalid emails.
+  # For each email that is invalid, assert_not false
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  # This test sets two users to have the same email address
+  # After saving one user, if the second user is invalid, assert_not
+  # false.
+  test "email addresses should be unique" do
+    dup_user = @user.dup
+    dup_user.email = @user.email.upcase
+    @user.save
+    assert_not dup_user.valid?
+  end
+
+
 end
